c9577e0e4e25a40a5fc0929fd9fd98bb2371b005
liuyufei <gabriel.liu@centling.com>
2016-11-02 21:27:31 +1300

    WTF elasticsearch for searching "" string

--- src/main/java/com/gabriel/service/util/ElasticSearchUtil.java
+++ src/main/java/com/gabriel/service/util/ElasticSearchUtil.java
@@ -10,10 +10,6 @@ public class ElasticSearchUtil {
     //http://stackoverflow.com/questions/25561981/find-documents-with-empty-string-value-on-elasticsearch/25562877#25562877
     //for the sake of elasticsearch deals with "" as the way of dealing null
     public static String getNullIfEmptyStr(String input){
-        if("".equals(input)){
-            return null;
-        }else{
-            return input;
-        }
+        return "".equals(input)?null:input;
     }
 }
